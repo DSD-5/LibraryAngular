680883863690a61fe0aba81ccb65d2212f7d69ea
Jesús Iparraguirre <jesusiparraguirre1998@gmail.com>
2020-11-29 13:32:43 -0500

    ejemplo

--- src/constantes.ts
+++ src/constantes.ts
@@ -8,4 +8,5 @@ export const environment = {
     CATALOGO : 'http://ec2-18-234-129-97.compute-1.amazonaws.com:8099/api/book/v1/catalog/retrieveBookCatalog',
     LOGIN : 'http://ec2-18-234-129-97.compute-1.amazonaws.com:8099/api/segurity/oauth/token',
     CARRITO : 'http://ec2-18-234-129-97.compute-1.amazonaws.com:8099/api/rentsales/v1/rent-sales/rentSalesBook',
+    Car: '',
   }
